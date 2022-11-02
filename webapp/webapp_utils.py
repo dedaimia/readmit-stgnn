845a45f6a4e9d09faa15fafbe472b893b6487b1d
Harris <Harris.Nicholas@mayo.edu>
2022-11-02 15:44:58 -0500

    fix namespace init
    Signed-off-by: Harris <Harris.Nicholas@mayo.edu>

--- webapp/webapp_utils.py
+++ webapp/webapp_utils.py
@@ -5,7 +5,7 @@ script_path = os.path.dirname(os.path.realpath(__file__))
 code_path = os.path.join(script_path, '..')
 
 class defaultInferenceArgs(SimpleNamespace):
-    def _init_ (self, edge_ehr_file=None, ehr_feature_file=None, demo_file=None):
+    def __init__ (self, edge_ehr_file=None, ehr_feature_file=None, demo_file=None):
         super().__init__(edge_ehr_file=edge_ehr_file, ehr_feature_file=ehr_feature_file, demo_file=demo_file)
 
         # Researcher set args - leaving separate to match formatting of run.sh
