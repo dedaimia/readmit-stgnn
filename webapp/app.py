c78851dc0d59ce3f5a3ad8b86b17a5056e233e88
Nick Harris <harris.nicholas@mayo.edu>
2022-11-17 16:33:47 -0600

    Update app.py
    Correct webapp typo

--- webapp/app.py
+++ webapp/app.py
@@ -135,7 +135,7 @@ def predict_and_convert_results(results_folder, status_file, status_df,  infer_a
             predictions = pd.DataFrame(pd.read_pickle(f)).set_index('node_indices')
         with open(os.path.join(code_path, 'node_mapping.pkl'), 'rb') as f:
             maps = pd.DataFrame(list(pd.read_pickle(f)))
-        final_preds = pd.mrge(predictions, maps, left_index=True, right_index=True)
+        final_preds = pd.merge(predictions, maps, left_index=True, right_index=True)
 
         final_preds.to_csv(os.path.join(results_folder, 'readmit-preds.csv'))
 
